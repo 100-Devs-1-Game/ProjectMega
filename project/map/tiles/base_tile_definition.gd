6da9f78e955082bed5f89918d8df19b22b7fabf1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-18 06:04:48 +0100

    Create tile definitions

---
+++ project/map/tiles/base_tile_definition.gd
@@ -0,0 +1,7 @@
+class_name BaseTileDefinition
+extends Resource
+
+@export var name: String
+
+var tile_source: int
+var atlas_coords: Vector2i 
---
+++ project/map/tiles/terrain_tile_definition.gd
@@ -0,0 +1,4 @@
+class_name TerrainTileDefinition
+extends BaseTileDefinition
+
+var walk_speed: float
