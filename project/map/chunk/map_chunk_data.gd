3ede3db57b5c70076302642753f8212727f9117c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-21 18:57:42 +0100

    Implement MapChunkData

---
+++ project/map/chunk/map_chunk_data.gd
@@ -0,0 +1,5 @@
+class_name MapChunkData
+
+var terrain_tiles: Dictionary[Vector2i, TerrainTileDefinition]
+
+var changes: TileChanges
---
+++ project/map/chunk/tile_changes.gd
@@ -0,0 +1,19 @@
+class_name TileChanges
+
+class Row:
+	enum Type { PLACE, REMOVE }
+	var type: Type
+	var tile_pos: Vector2i
+	var layer: BaseTileDefinition.Type
+	
+	func _init(p_type: Type, pos: Vector2i, p_layer: BaseTileDefinition.Type):
+		type = p_type
+		tile_pos = pos
+		layer = p_layer
+
+
+var rows: Array[Row]
+
+
+func add_row(row: Row):
+	rows.append(row)
