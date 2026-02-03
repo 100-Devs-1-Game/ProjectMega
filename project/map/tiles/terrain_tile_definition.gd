c776821053f313c5a8e8ca291b8a1f6793307a14
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-02-03 11:08:45 +0100

    Add deserializer

--- project/map/tiles/terrain_tile_definition.gd
+++ project/map/tiles/terrain_tile_definition.gd
@@ -1,7 +1,12 @@
 class_name TerrainTileDefinition
 extends BaseTileDefinition
 
-var walk_speed: float
+var walk_speed: float = 1.0
+
+
+func deserialize(data: Dictionary):
+	super(data)
+	walk_speed = data.get("walk_speed", 1.0)
 
 
 func get_layer()-> Layer:
